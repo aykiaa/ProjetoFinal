2d5b3cc61b636aaa4d82b9ebf4d4c3710b543881
pedrobacelar28 <pedrobrigueira1028@gmail.com>
2023-11-24 20:57:51 -0300

    Primeiro commit main

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include "Filme.hpp"
+#include "dvd.hpp"
+#include "cliente.hpp"
+#include "fita.hpp"
+#include "Locadora.hpp"
+
+using namespace std;
+
+int main(){
+    string comando;
+    while(1){
+        cin>>comando;
+        if(comando=="LA"){
+            
+        }
+        else if(comando=="CF"){
+
+        }
+        else if(comando=="RF"){
+            
+        }
+        else if(comando=="LF"){
+            
+        }
+        else if(comando=="CC"){
+            
+        }
+        else if(comando=="RC"){
+            
+        }
+        else if(comando=="LC"){
+            
+        }
+        else if(comando=="AL"){
+            
+        }
+        else if(comando=="DV"){
+            
+        }
+        else if(comando=="FS"){
+            
+        }
+        
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+

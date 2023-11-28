7209557fda3d9e6b32f84a86b741ff2ce8e9d32d
Luiz <145346778+Luiz3112@users.noreply.github.com>
2023-11-27 22:39:50 -0300

    Filme.cpp

--- Filme.cpp
+++ Filme.cpp
@@ -1,3 +1,11 @@
+/*/ Define a implementação da classe Filme.
+A classe possui um construtor que inicializa os atributos da instância (_id, _titulo e _qtd_disp) com os valores fornecidos como argumentos.
+O seu destrutor é usado para limpeza quando um objeto Filme é destruído.
+A classe também fornece métodos públicos de acesso para recuperar informações sobre um filme. Os métodos:
+1- get_id() retorna o identificador único do filme, 
+2- get_titulo() retorna o título do filme, 
+3- get_qtdDisp() retorna a quantidade disponível de cópias desse filme.
+Essa classe serve como uma base para as classes derivadas, como DVD e Fita, que adicionam funcionalidades específicas para diferentes tipos de filmes./*/
 #include "Filme.hpp"
 
 

d37f726a6f466dd8ca9e18797c0c98b5ad3a3ffa
Luiz <145346778+Luiz3112@users.noreply.github.com>
2023-11-27 23:14:32 -0300

    Update cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -1,5 +1,7 @@
-/*/Definição da classe Cliente serve como uma representação básica de um cliente, armazenando informações como CPF e nome.
+/*/ Definição da classe Cliente serve como uma representação básica de um cliente, armazenando informações como CPF e nome.
+
 Os métodos de acesso (get_cpf() e get_nome()) permitem recuperar essas informações de um objeto Cliente./*/
+
 #include "cliente.hpp"
 #include <algorithm>
 

d9931deb63888d00edc5660f587663189f05b36f
Luiz <145346778+Luiz3112@users.noreply.github.com>
2023-11-28 08:48:14 -0300

    Update fita.cpp

--- fita.cpp
+++ fita.cpp
@@ -1,4 +1,4 @@
-/*/implementação da classe Fita.
+/** Implementação da classe Fita.
 
 O construtor inicializa os atributos da classe, herdando alguns de sua classe base Filme e introduzindo um novo atributo privado chamado _isRebobinada (este atributo é randomizado no momento da criação da fita, determinando se a fita está ou não rebobinada).
 
@@ -11,7 +11,7 @@ O método get_isRebobinada() retorna o estado da fita, informando se ela está r
 A função calc_valor_locacao(int dias) const calcula o valor de locação da fita com base no número de dias especificado.
 
 1- Se a fita estiver rebobinada, o custo é de 5.0 reais;
-2- Caso contrário, o custo é de 7.0 reais, considerando uma multa de 2.0 reais por não rebobinar a fita./*/
+2- Caso contrário, o custo é de 7.0 reais, considerando uma multa de 2.0 reais por não rebobinar a fita.**/
 
 #include "fita.hpp"
 #include <cstdlib>

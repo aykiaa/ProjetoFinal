529fca8c204b8814d55f542403fc569fd0eceeb0
Luiz <145346778+Luiz3112@users.noreply.github.com>
2023-11-29 09:20:55 -0300

    Últimas alterações de descrição fita.hpp

--- fita.hpp
+++ fita.hpp
@@ -1,28 +1,26 @@
-/// Cabecalho que define a estrutura da classe Fita, apresentando as funcionalidades específicas para fitas no sistema de locadora da filmes como:
-
-/// 1- Id
-/// 2- Título
-/// 3- Quantidade
-/// 4- Valor
-/// 5- Rebobinação
+/// Cabecalho da classe Fita.
 
 #ifndef FITA_H
 #define FITA_H
 #include "Filme.hpp"
-
+/// Classe Fita é uma subclasse de Filme.
 class Fita : public Filme {
 
 private:
-    bool _isRebobinada;
+    bool _isRebobinada; /// Indica se a fita está rebobinada.
 
 public:
+/// Construtor da classe Fita.
     Fita(int id, string titulo, int qtd_disp);
+/// Destrutor da classe Fita.
     ~Fita();
-
+/// Método que retorna a categoria.
     virtual string get_categoria() override;
+/// Método que retorna o tipo do filme, no caso fita.
     virtual string get_tipo() override;
+/// Método que verifica se a fita está rebobinada.
     bool get_isRebobinada() const;
-    
+/// Método que calcula o valor da locação da fita com base na quantidade de dias.    
     float calc_valor_locacao (int dias) const override;
 
 };

8717baa76bf70310af752ca796649573ef3c2783
Luiz <145346778+Luiz3112@users.noreply.github.com>
2023-11-29 08:27:10 -0300

    Últimas alterações de descrição Filme.hpp

--- Filme.hpp
+++ Filme.hpp
@@ -1,15 +1,5 @@
 /// Cabeçalho da classe abstrata Filme, a qual possui membros privados, incluindo um identificador unico, um titulo e a quantidade disponivel de copias.
 
-/// O seu construtor Filme(int id, string titulo, int qtd_disp) e responsavel por inicializar esses atributos.
-
-/// O destrutor virtual e declarado, indicando que a classe e abstrata e pode conter metodos virtuais puros.
-
-/// A classe Filme inclui metodos publicos de acesso como:
-
-/// 1- get_id() para obter o identificador
-/// 2- get_titulo() para obter o titulo
-/// 3- get_qtdDisp() para obter a quantidade disponivel.
-
 /// Alem disso, a classe declara metodos virtuais puros get_categoria() e calc_valor_locacao(int dias) const, indicando que as classes derivadas devem implementar esses metodos.
 
 #ifndef FILME_H
@@ -28,14 +18,14 @@ private:
     int _qtd_disp;
 
 public:
-    Filme(int id, string titulo, int qtd_disp);
-    virtual ~Filme();
+    Filme(int id, string titulo, int qtd_disp); /// Construtor que realiza a iniciação e inclusão de metodos publicos de acesso.
+    virtual ~Filme(); /// O destrutor virtual e declarado, indicando que a classe e abstrata e pode conter metodos virtuais puros.
     
     virtual string get_categoria() = 0;
     virtual string get_tipo() = 0;
-    int get_id() const;
-    string get_titulo() const;
-    int get_qtdDisp() const;
+    int get_id() const; /// get_id() para obter o identificador
+    string get_titulo() const; /// get_titulo() para obter o titulo
+    int get_qtdDisp() const; /// get_qtdDisp() para obter a quantidade disponivel.
 
     virtual float calc_valor_locacao (int dias) const = 0;
 };

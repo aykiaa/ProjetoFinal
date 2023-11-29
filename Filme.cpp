a849e09bb4637f79eb0b62f4a01b989b4fb6eb21
Luiz <145346778+Luiz3112@users.noreply.github.com>
2023-11-29 08:22:59 -0300

    Últimas alterações de descrição Filme.cpp

--- Filme.cpp
+++ Filme.cpp
@@ -1,32 +1,22 @@
 /// Define a implementacao da classe Filme.
 
-/// A classe possui um construtor que inicializa os atributos da instancia (_id, _titulo e _qtd_disp) com os valores fornecidos como argumentos.
-
-/// O seu destrutor e usado para limpeza quando um objeto Filme e destruido.
-
-/// A classe também fornece metodos publicos de acesso para recuperar informacoes sobre um filme. Os metodos:
-
-/// 1- get_id() retorna o identificador unico do filme, 
-/// 2- get_titulo() retorna o titulo do filme, 
-/// 3- get_qtdDisp() retorna a quantidade disponivel de copias desse filme.
-
 /// Essa classe serve como uma base para as classes derivadas, como DVD e Fita, que adicionam funcionalidades especificas para diferentes tipos de filmes.
 
 #include "Filme.hpp"
 
 
-Filme::Filme(int id, string titulo, int qtd_disp) : _id(id), _titulo(titulo), _qtd_disp(qtd_disp){};
+Filme::Filme(int id, string titulo, int qtd_disp) : _id(id), _titulo(titulo), _qtd_disp(qtd_disp){}; /// A classe possui um construtor que inicializa os atributos da instancia (_id, _titulo e _qtd_disp) com os valores fornecidos como argumentos.
 
-Filme::~Filme(){};
+Filme::~Filme(){};/// O seu destrutor e usado para limpeza quando um objeto Filme e destruido.
 
-int Filme::get_id() const{ /// Armazena o id
+int Filme::get_id() const{ /// Armazena e retorna o identificador unico do filme. 
     return _id;
 }
 
-string Filme::get_titulo() const{ /// Armazena o titulo
+string Filme::get_titulo() const{ /// Armazena e retorna o titulo do filme.
     return _titulo;
 }
 
-int Filme::get_qtdDisp() const{ /// Armazena o quantidade disponivel
+int Filme::get_qtdDisp() const{ /// Armazena e retorna a quantidade disponivel de copias desse filme.
     return _qtd_disp;
 }
